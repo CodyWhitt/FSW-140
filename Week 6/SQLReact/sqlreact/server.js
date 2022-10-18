04c672b60287e884adf7abd4a6e5b80507ab27d8
CodyWhitt <95008570+CodyWhitt@users.noreply.github.com>
2022-10-18 18:25:51 -0500

    initial commit

---
+++ Week 6/SQLReact/sqlreact/server.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const mysql = require('mysql')
+
+const db = mysql.createConnection({
+    user     : 'root',
+    password : 'Ctphaxf7',
+    database : 'avengers'
+})
+
+
+//Connect
+db.connect((err) => {
+    if(err){
+        console.log(err)
+    }
+    console.log('MySql Connected...')
+})
+
+const app = express()
+
+app.get('/additem')
+
+
+// // Create DB
+// app.get('/createdb', (req, res) => {
+//         let sql = 'CREATE DATABASE nodemysql'
+//         db.query(sql, (err, result) => {
+//             if(err) console.log(err)
+//             console.log(result)
+//             res.send('Database created...')
+//         })
+// })
+
+// // Create table
+// app.get('/createpoststable', (req,res) => {
+//     let sql = 'CREATE TABLE posts(id int AUTO_INCREMENT, title VARCHAR(255), BODY VARCHAR(255), PRIMARY KEY (id)'
+//     db.query(sql, (err, result) => {
+//         if (err) console.log(err)
+//         res.send('Posts table created')
+//     })
+// })
+
+app.listen('3000', ()=> {
+  console.log('server started on port 3000')
+})  
